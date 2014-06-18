24c836abb63ceaba4d80152b4178a0fee4186c4a
Divesh Goyal <DGoyal@esri.com>
2014-06-18 16:14:15 -0700

    Changed project to use storyboards

--- Add Map Tutorial/MyFirstMapApp/AppDelegate.m
+++ Add Map Tutorial/MyFirstMapApp/AppDelegate.m
@@ -23,15 +23,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        self.viewController = [[ViewController alloc] initWithNibName:@"ViewController_iPhone" bundle:nil];
-    } else {
-        self.viewController = [[ViewController alloc] initWithNibName:@"ViewController_iPad" bundle:nil];
-    }
-    self.window.rootViewController = self.viewController;
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
